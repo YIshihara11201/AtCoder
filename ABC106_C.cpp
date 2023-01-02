a1eb54714ec9ba2a27414327527432a888b33190
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2023-01-02 14:18:31 -0800

    Create ABC106_C.cpp

---
+++ ABC106_C.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int n;
+  cin >> n;
+  
+  int res = 0;
+  for(int i=1; i<=n; i++) {
+    if(i%2 == 0) {
+    	continue;
+    }
+   	int cnt = 0;
+    for(int j=1; j<=i; j++) {
+    	if(i%j==0) {
+        	cnt++;
+          	if(cnt==9) {
+            	break;
+            }
+        }
+    }
+    if(cnt == 8) {
+    	res++;
+    }
+  }
+  
+  cout << res << endl;
+}
