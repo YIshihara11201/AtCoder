6430607cd9b4145694a35c0737fe141b0525138c
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2023-01-02 13:51:11 -0800

    Create ABC87_B.cpp

---
+++ ABC87_B.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a, b, c, x;
+  cin >> a >> b >> c >> x;
+  
+  int res = 0;
+  for(int i=0; i<=a; i++) {
+    int curr_500 = 500*i;
+    if(curr_500>x) {
+    	break;
+    }
+    for(int j=0; j<=b; j++) {
+      int curr_500_100 = curr_500 + 100*j;
+      if(curr_500_100 > x) {
+      	break;
+      }
+      if((x-curr_500_100)/50 <=c && (x-curr_500_100)/50 >= 0) {
+        res += 1;
+      }
+    }
+  }
+  
+  cout << res << endl;
+}
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b, c, x;
+  cin >> a >> b >> c >> x;
+  
+  int res = 0;
+  for(int i=0; i<=a; i++) {
+    int curr_500 = 500*i;
+    if(curr_500>x) {
+    	break;
+    }
+    for(int j=0; j<=b; j++) {
+      int curr_500_100 = curr_500 + 100*j;
+      if(curr_500_100 > x) {
+      	break;
+      }
+      if((x-curr_500_100)/50 <=c && (x-curr_500_100)/50 >= 0) {
+        res += 1;
+      }
+    }
+  }
+  
+  cout << res << endl;
+}
