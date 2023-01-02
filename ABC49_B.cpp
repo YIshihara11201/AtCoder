d1ee6d39f734dd636988ce64425ce009b1dae62b
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2023-01-02 14:05:13 -0800

    Create ABC49_B.cpp

---
+++ ABC49_B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int n, m;
+  cin >> n >> m;
+  
+  string curr;
+  for(int i=0; i<n; i++) {
+	cin >> curr;
+    cout << curr << endl;
+    cout << curr << endl;
+  }
+}
