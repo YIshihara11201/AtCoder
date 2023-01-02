946471a95d13129bbcd330ca3f422c9a278a3adf
YIshihara11201 <98417271+YIshihara11201@users.noreply.github.com>
2023-01-02 13:38:39 -0800

    Create ABC161_C.cpp

---
+++ ABC161_C.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int64_t N, K;
+  cin >> N >> K;
+  
+  cout << min(N%K, abs(N%K-K)) << endl;
+}
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int64_t N, K;
+  cin >> N >> K;
+  
+  cout << min(N%K, abs(N%K-K)) << endl;
+}
